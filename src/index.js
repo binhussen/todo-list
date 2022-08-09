f1ee4496f71efd60535e6423a5066c7920a12ef6
binhussen <mhdhsna@gmail.com>
2022-08-09 16:56:47 +0300

    Add index.js with array of tasks

---
+++ src/index.js
@@ -0,0 +1,18 @@
+/** @format */
+const tasks = [
+	{
+		description: "Read Official Documentation",
+		completed: false,
+		index: 0,
+	},
+	{
+		description: "Read js guide",
+		completed: true,
+		index: 1,
+	},
+	{
+		description: "Training on js projects",
+		completed: false,
+		index: 2,
+	},
+];
