996f8196b47ccc1f6eef6332448ce89383e7a7f3
binhussen <mhdhsna@gmail.com>
2022-08-09 19:51:21 +0300

    Update-style

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
 /** @format */
-import "./style.css";
+import './style.css';
 
 const tasks = [
-	{
-		description: "Read Official Documentation",
-		completed: false,
-		index: 0,
-	},
-	{
-		description: "Read js guide",
-		completed: true,
-		index: 1,
-	},
-	{
-		description: "Training on js projects",
-		completed: false,
-		index: 2,
-	},
+  {
+    description: 'Read Official Documentation',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Read js guide',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'Training on js projects',
+    completed: false,
+    index: 2,
+  },
 ];
 
-const tasksList = document.querySelector(".list");
+const tasksList = document.querySelector('.list');
 
 tasks.sort((a, b) => a.index - b.index);
 
 tasks.forEach((task) => {
-	tasksList.innerHTML += `
+  tasksList.innerHTML += `
         <li class="task">
             <input class="checkbox" type="checkbox" ${
-							task.completed ? "checked" : "unchecked"
-						}>
+  task.completed ? 'checked' : 'unchecked'
+}>
             <p class="disc">${task.description}</p>
-            <button class="btn btn-select"><i class="fa fa-ellipsis-v" aria-hidden="true"></i></button>
+            <button type="button" class="btn btn-select"><i class="fa fa-ellipsis-v" aria-hidden="true"></i></button>
         </li>
   `;
 });
