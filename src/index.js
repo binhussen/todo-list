f955dda4809a9ff6b0d9ca7aa07d27e8d8e8a5f5
binhussen <mhdhsna@gmail.com>
2022-08-10 22:33:49 +0300

    Update diplay lists

--- src/index.js
+++ src/index.js
@@ -11,19 +11,3 @@ const tasks = [];
 const todo = new Todo();
 
 btnAdd.addEventListener("click", todo.addTask);
-
-const tasksList = document.querySelector(".list");
-
-tasks.sort((a, b) => a.index - b.index);
-
-tasks.forEach((task) => {
-	tasksList.innerHTML += `
-        <li class="task">
-            <input class="checkbox" type="checkbox" ${
-							task.completed ? "checked" : "unchecked"
-						}>
-            <p class="disc">${task.description}</p>
-            <button type="button" class="btn btn-select"><i class="fa fa-ellipsis-v" aria-hidden="true"></i></button>
-        </li>
-  `;
-});
--- src/operation.js
+++ src/operation.js
@@ -1,6 +1,7 @@
 /** @format */
 
 const input = document.querySelector(".input");
+const tasksList = document.querySelector(".list");
 export default class Todo {
 	tasks;
 
@@ -16,5 +17,22 @@ export default class Todo {
 			completed: false,
 		};
 		this.tasks.push(task);
+		this.displayTasks();
+	};
+
+	displayTasks = () => {
+		this.tasks.sort((a, b) => a.index - b.index);
+		tasksList.innerHTML = "";
+		this.tasks.forEach((task, i) => {
+			tasksList.innerHTML += `
+            <li class="task">
+                <input class="checkbox" type="checkbox" ${
+									task.completed ? "checked" : "unchecked"
+								}>
+            <p class="disc">${task.description}</p>
+            <button type="button" class="btn btn-select"><i class="fa fa-ellipsis-v" aria-hidden="true"></i></button>
+        </li>
+  `;
+		});
 	};
 }
