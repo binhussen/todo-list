ccd7187d5a8531959ecf1c1a68584376efbf7e78
binhussen <mhdhsna@gmail.com>
2022-08-10 21:37:15 +0300

    clean hard coded

--- src/index.js
+++ src/index.js
@@ -1,34 +1,18 @@
 /** @format */
-import './style.css';
+import "./style.css";
 
-const tasks = [
-  {
-    description: 'Read Official Documentation',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Read js guide',
-    completed: true,
-    index: 1,
-  },
-  {
-    description: 'Training on js projects',
-    completed: false,
-    index: 2,
-  },
-];
+const tasks = [];
 
-const tasksList = document.querySelector('.list');
+const tasksList = document.querySelector(".list");
 
 tasks.sort((a, b) => a.index - b.index);
 
 tasks.forEach((task) => {
-  tasksList.innerHTML += `
+	tasksList.innerHTML += `
         <li class="task">
             <input class="checkbox" type="checkbox" ${
-  task.completed ? 'checked' : 'unchecked'
-}>
+							task.completed ? "checked" : "unchecked"
+						}>
             <p class="disc">${task.description}</p>
             <button type="button" class="btn btn-select"><i class="fa fa-ellipsis-v" aria-hidden="true"></i></button>
         </li>
