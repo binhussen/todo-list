549b9d08f6e1bb4022b7802829a9882a84bf3bbb
binhussen <mhdhsna@gmail.com>
2022-08-18 15:03:13 +0300

    Add test for dom task lists

--- src/operation.test.js
+++ src/operation.test.js
@@ -5,12 +5,19 @@
 import Todo from './operation.js';
 
 describe('Manipulate Add and delete Tasks', () => {
-  const todo = new Todo();
-  test('Add a Task', () => {
-    const taskLength = todo.tasks.length;
+  document.body.innerHTML = '<div> <ul class="list"></ul> </div>';
 
+  const todo = new Todo();
+  const taskLength = todo.tasks.length;
+
+  test('Add a Task', () => {
     todo.addTask('Test 1');
     expect(todo.tasks).toHaveLength(taskLength + 1);
+  });
+
+  test('Adding task to DOM', () => {
+    const list = document.querySelectorAll('.task');
+    expect(list).toHaveLength(taskLength + 1);
   });
 
   test('Remove a Task', () => {
@@ -18,5 +25,13 @@ describe('Manipulate Add and delete Tasks', () => {
     const taskLength = todo.tasks.length;
     todo.removeTask(0);
     expect(todo.tasks).toHaveLength(taskLength - 1);
+  });
+
+  test('Remove task from DOM', () => {
+    todo.addTask('Test 2');
+    const taskLength = todo.tasks.length;
+    todo.removeTask(0);
+    const list = document.querySelectorAll('.task');
+    expect(list).toHaveLength(taskLength - 1);
   });
 });
